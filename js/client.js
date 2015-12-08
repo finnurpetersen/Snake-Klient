657c880c542f91a45de7f8b0346c3e7fcfd08d25
finnurpetersen <finnurpetersen@gmail.com>
2015-12-08 22:46:55 +0100

    ommøbeleret rækkefølgeen som den giver bedre mening

--- js/client.js
+++ js/client.js
@@ -4,12 +4,10 @@
 
 //login function
 $(document).ready(function () {
-
     $(".login-button").click(function () {
 
         var password = $('#password').val();
         var username = $('#username').val();
-
 
         var data = {
             username: username,
@@ -20,6 +18,7 @@ $(document).ready(function () {
             type: "post",
             url: "http://localhost:8888/api/login",
             data: JSON.stringify(data),
+
             success: function (data, status, xhr) {
                 console.log(data, status, xhr);
 
@@ -31,19 +30,13 @@ $(document).ready(function () {
                 console.log(err);
 
                 alert('Something went wrong')
-
             }
-
-
         });
-
     });
-
 });
 
-//create-user function
+//create user function
 $(document).ready(function () {
-
     $(".create-user").click(function () {
 
         var Firstname = $('#firstName').val();
@@ -63,6 +56,7 @@ $(document).ready(function () {
             type: 'POST',
             url: 'http://localhost:8888/api/users',
             data: JSON.stringify(user),
+
             success: function (data, status, xhr) {
                 console.log(data, status, xhr);
 
@@ -72,18 +66,14 @@ $(document).ready(function () {
             error: function (err) {
                 console.log(err);
 
-
                 alert('Something went wrong');
-
             }
         });
     });
-
 });
 
-//create-game function
+//create game function
 $(document).ready(function () {
-
     $(".create-game").click(function () {
 
         var createGame = {
@@ -93,13 +83,13 @@ $(document).ready(function () {
                 controls: $('#controls').val()
             },
             mapSize: $('#mapSize').val()
-
         };
 
         $.ajax({
             type: 'POST',
             url: 'http://localhost:8888/api/games',
             data: JSON.stringify(createGame),
+
             success: function (data, status, xhr) {
                 console.log(data, status, xhr);
 
@@ -109,20 +99,19 @@ $(document).ready(function () {
             error: function (err) {
                 console.log(err);
 
-
                 alert('Something went wrong');
-
             }
         });
     });
 });
 
-//join-game table
+//join open game table
 $(document).ready(function () {
 
     $.ajax({
         type: "GET",
         url: "http://localhost:8888/api/games/open",
+
         success: function (data, status, xhr) {
             console.log(data, status, xhr);
 
@@ -141,14 +130,11 @@ $(document).ready(function () {
         error: function (err) {
             console.log(err);
         }
-
     });
 });
 
+//join games function
 $(document).ready(function () {
-    //join games
-
-
     $(".join-game").click(function () {
 
         var game = $("#gameId").val();
@@ -161,93 +147,115 @@ $(document).ready(function () {
                 id: $.session.get('userId')
             }
         };
-
         console.log(JSON.stringify((joinGameNow)));
 
         $.ajax({
             type: "POST",
             url: "http://localhost:8888/api/games/join",
             data: JSON.stringify(joinGameNow),
-            success:function(data) {
+            success:function(data, status, xhr) {
+                console.log(data, status, xhr)
 
                 alert("Game ready to start");
-
                 window.location.href = '../html/start-game.html';
-
+                $.session.set('gameId', game);
             },
             error: function (err) {
                 console.log(err);
                 alert('Game ready to start');
                 window.location.href = '../html/start-game.html';
                 $.session.set('gameId', game);
-
             }
         });
     });
 });
 
-
-
-//Delete-game funktion
+// Start game function
 $(document).ready(function () {
+    $(".start-game").click(function () {
 
-    $(".See-game").click(function () {
-
-        var gameId = $("#DeleteID").val();
-
-$.ajax({
-    type: "GET",
-    url: "http://localhost:8888/api/games/pending/" +  $("#DeleteID").val(),
-    success: function (data, status, xhr) {
-        console.log(data, status, xhr);
-
-        data.forEach(function (item) {
-
-            var table2 = '<tr>' +
-                '<td>' + item.gameId + '</td>' +
-                '<td>' + item.host.id + '</td>' +
-                '<td>' + item.name + '</td>' +
-                '<td>' + item.created + '</td></tr>';
-
-            $('#YourGameTable').append(table2);
-        });
-    },
-    error: function (err) {
-        console.log(err);
-    }
-
-});
-    });
-
-});
-
-
-    $(".Delete-game").click(function () {
-
-        var gameId = $("#gameId").val();
-
-
+        var startGame = {
+            gameId: $.session.get("gameId"),
+            opponent: {
+                controls: $('#OpponentControls').val()
+            }
+        };
         $.ajax({
             type: 'POST',
-            url: 'http://localhost:8888/api/games/' + $("#gameId").val(),
-            data: JSON.stringify(gameId),
+            url: 'http://localhost:8888/api/games/start/',
+            data: JSON.stringify(startGame),
+
             success: function (data, status, xhr) {
-                console.log(data, status, xhr)
+                console.log(data, status, xhr);
 
-                alert('Game is Deleted');
-
+                alert('Game is Finnished');
                 window.location.href = '../html/menu.html';
             },
             error: function (err) {
                 console.log(err);
 
-
                 alert('Something went wrong');
-
             }
-
         });
     });
+});
+
+//open pending game table
+$(document).ready(function () {
+    $(".See-game").click(function () {
+
+        var gameId = $("#DeleteID").val();
+
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:8888/api/games/pending/" +  $("#DeleteID").val(),
+
+            success: function (data, status, xhr) {
+                console.log(data, status, xhr);
+
+                data.forEach(function (item) {
+
+                    var table2 = '<tr>' +
+                        '<td>' + item.gameId + '</td>' +
+                        '<td>' + item.host.id + '</td>' +
+                        '<td>' + item.name + '</td>' +
+                        '<td>' + item.created + '</td></tr>';
+
+                    $('#YourGameTable').append(table2);
+                });
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    });
+});
+
+//Delete game table
+$(document).ready(function () {
+    $(".Delete-game").click(function () {
+
+        var gameId = $("#gameId").val();
+
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:8888/api/games/' + $("#gameId").val(),
+            data: JSON.stringify(gameId),
+
+            success: function (data, status, xhr) {
+                console.log(data, status, xhr)
+
+                alert('Game is Deleted');
+                window.location.href = '../html/menu.html';
+            },
+            error: function (err) {
+                console.log(err);
+
+                alert('Something went wrong');
+            }
+        });
+    });
+});
 
 //Highscre table
 $(document).ready(function () {
@@ -255,6 +263,7 @@ $(document).ready(function () {
     $.ajax({
         type: "GET",
         url: "http://localhost:8888/api/scores",
+
         success: function (data, status, xhr) {
             console.log(data, status, xhr);
 
@@ -272,40 +281,7 @@ $(document).ready(function () {
         error: function (err) {
             console.log(err);
         }
-
     });
 });
 
 
-$(document).ready(function () {
-$(".start-game").click(function () {
-
-    var startGame = {
-        gameId: $.session.get("gameId"),
-        opponent: {
-            controls: $('#OpponentControls').val()
-        }
-    };
-
-    $.ajax({
-        type: 'POST',
-        url: 'http://localhost:8888/api/games/start/',
-        data: JSON.stringify(startGame),
-        success: function (data, status, xhr) {
-            console.log(data, status, xhr);
-
-
-            alert('Game is Finnished');
-        },
-        error: function (err) {
-            console.log(err);
-
-
-            alert('Something went wrong');
-
-        }
-    });
-
-
-});
-});
